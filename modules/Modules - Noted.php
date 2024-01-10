3c97abd1dc959b4b1a0e598ec615107b74f53bf6
ogisetiawan <ogisetiawan21@gmail.com>
2024-01-10 12:04:15 +0700

    ✅ add Menerapkan PropTypes pada Aplikasi Kontak

--- modules/Modules - Noted.php
+++ modules/Modules - Noted.php
@@ -1,1 +1,9 @@
 <?php
+//@ Property Validation
+//? Dalam memvalidasi properti component, React merekomendasikan kita untuk menggunakan tools bernama PropTypes
+//? agar kita tidak perlu menulis secara manual menggunakan if untuk memvalidasi type data
+//? PropTypes.arrayOf; untuk validasi array, dengan type spesifik ( string/number/bool)
+//? PropTypes.objectOf; untuk validasi object, dengan type yg sama dlm satu obj ( string/number/bool)
+//? PropTypes.oneOf: Nilai props yang diberikan pada component harus salah satu nilai sesuai yang didefinisikan pada PropTypes.oneOf.
+//? PropTypes.func: Nilai props yang diberikan pada component harus bertipe function.
+//? dll
