d28b1b59119d8c9bda6fe0549f75139ee87b4b23
ogisetiawan <ogisetiawan21@gmail.com>
2024-01-15 15:07:59 +0700

    ✅ add modules noted React Router

--- modules/Modules - Noted.php
+++ modules/Modules - Noted.php
@@ -7,3 +7,19 @@
 //? PropTypes.oneOf: Nilai props yang diberikan pada component harus salah satu nilai sesuai yang didefinisikan pada PropTypes.oneOf.
 //? PropTypes.func: Nilai props yang diberikan pada component harus bertipe function.
 //? dll
+
+//@ React Router
+//? SPA, akan mengunduh seluruh konten website sekaligus, jdi web tidak akan render lg ketika pindah halaman
+//? tetapi biasanya konten yang diunduh hanya konten statis yang esensial dalam menampilkan UI ( header, sidebar dll)
+//~ React Router component
+/// BrowserRouter; berperan sebagai pembungkus (wrapper) | <BrowserRouter>
+/// Link; sama seperti tag a | <Link to={user.id}>{user.name}</Link>
+/// Routes dan Route; mendefinisikan komponen atau halaman apa yang ditampilkan berdasarkan URL | <Route path="/" element={<Dashboard />} />
+//~ Path dan Query Parameter
+//? path; menyisipkan sebuah data dengan memanfaatkan URL, jadi seperti dynamic param
+/// <Route path="/company/:name" element={<CompanyDetailPage />} />
+/// param name diambil dari component companyDetailPage
+//? query param; url yg memiliki 2 param, /company/search?name=dicoding&location=bandung
+//? useParams(): mengembalikan objek yang nilai propertinya merupakan path parameter dari URL yang aktif.
+//? useNavigate(): mengembalikan fungsi yang digunakan untuk menavigasi halaman secara terprogram (programatically).
+//? hooks, tidak dapat digunakan pada class, hanya pada functional component
